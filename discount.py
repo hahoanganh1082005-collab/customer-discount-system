5046bda908390b0eade80e34faaa9349c136ac5b
Hanh <hahoanganh1082005@gmail.com>
2026-06-08 20:22:37 +0700

    sss

---
+++ discount.py
@@ -0,0 +1,5 @@
+def calculate_discount(total_amount):
+    """Return discount rate based on total customer spend."""
+    if total_amount >= 60000000:
+        return 0.1
+    return 0
--- test_discount.py
+++ test_discount.py
@@ -1,6 +1,10 @@
 from discount import calculate_discount
 
-def test_vip_customer():
-    assert calculate_discount(60000000)==0.1
-def test_normal_customer():
-    assert calculate_discount(30000000)==0
+def test_TC01_vip_customer():
+    assert calculate_discount(60000000) == 0.1
+
+def test_TC02_normal_customer():
+    assert calculate_discount(30000000) == 0
+
+def test_TC03_boundary_non_vip_customer():
+    assert calculate_discount(59999999) == 0
